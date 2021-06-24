c9336201c45993577411e4f2225e895e7e5d9a28
zain-mzb <comsatsbscs@gmail.com>
2021-06-24 12:51:43 +0500

    Summary

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(config.get("db"), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
