2f6307049c9b75f14f3fb61a0962f27820ea84e7
zain-mzb <comsatsbscs@gmail.com>
2021-06-24 13:15:32 +0500

    Summary

--- flight/src/services/GenericService.js
+++ flight/src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/api/";
+axios.defaults.baseURL = process.env.PORT || "http://localhost:4000/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
   constructor() {}
